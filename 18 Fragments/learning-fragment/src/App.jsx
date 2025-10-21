444123ee3dbcf232c1c9a8f45a74e76f1ff9fa0e
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-10-21 15:24:09 +0530

    learned about conditional rendering

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -5,10 +5,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let foodItems =['Dal','Green Vegetables','Roti','Egg','Milk']
+  let foodItems =[]
   return (
     <React.Fragment>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 && <h3>I am stil hungry</h3>}
       <ul className="list-group">
         {foodItems.map((item) => <li key={item} className="list-group-item">{item}</li>)}
       </ul>
