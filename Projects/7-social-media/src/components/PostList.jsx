33e4f3fa161944a6b0a2aafce3633b2602fcdf95
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-11-03 19:58:29 +0530

    completed the social media project

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -5,7 +5,6 @@ import { PostList as PostListData } from "../store/post-list-store";
 const PostList=()=>{
     
     const {postList} = useContext(PostListData)
-    console.log(postList)
     return(
         <>
         {postList.map((post) => <Post key={post.id} post={post}/>)}
