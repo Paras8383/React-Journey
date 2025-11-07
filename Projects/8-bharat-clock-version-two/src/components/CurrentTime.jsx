c7f6ce3f7f6cbad4d1e64fc5689c8b9722c4978f
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-11-07 18:25:36 +0530

    optimized and upgraded the bharat clock app using useEffect

--- Projects/8-bharat-clock-version-two/src/components/CurrentTime.jsx
+++ Projects/8-bharat-clock-version-two/src/components/CurrentTime.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function CurrentTime(){ 
-    let time = new Date();
+    const [time, setTime] = useState(new Date())
+    useEffect(() => {
+        const intervalId = setInterval(() =>{
+            setTime(new Date())
+        },1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [])
+
     return(
         
         <p className="lead">The current time is: {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
