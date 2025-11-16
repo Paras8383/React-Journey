c89f922a44758b574357a2b505dc7637e720da1f
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-11-16 18:56:59 +0530

    started making the counter project which will use react redux

---
+++ 52 redux/Counter-React-Redux/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import DisplayCounter from "./components/DisplayCounter";
+import Container from "./components/container";
+import Controls from "./components/Controls";
+function App() {
+  return (
+    <>
+    <center className="px-4 py-5 my-5 text-center">
+    <Container>
+    <Header></Header>       
+        {" "}
+        <h1 className="display-5 fw-bold text-body-emphasis">
+          Centered hero
+        </h1>{" "}
+        <div className="col-lg-6 mx-auto">
+          {" "}
+          <DisplayCounter/>
+          <Controls/>
+        </div>{" "}     
+      </Container>
+      </center> 
+    </>
+  );
+}
+
+export default App;
