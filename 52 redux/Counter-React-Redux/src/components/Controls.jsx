a00a8359bd0590ccf459509b121309681d92ccc4
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-11-18 21:59:39 +0530

    completed the counter project using react redux

--- 52 redux/Counter-React-Redux/src/components/Controls.jsx
+++ 52 redux/Counter-React-Redux/src/components/Controls.jsx
@@ -1,11 +1,24 @@
+import { useDispatch } from "react-redux";
+
 const Controls = () => {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch({type:'INCREMENT'})
+  }
+
+  const handleDecrement = () => {
+    dispatch({type:'DECREMENT'})
+  }
   return (
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
       {" "}
-      <button type="button" class="btn btn-primary">
+      <button type="button" class="btn btn-primary"
+      onClick={handleIncrement}>
         +1
       </button>
-      <button type="button" class="btn btn-success">
+      <button type="button" class="btn btn-success"
+      onClick={handleDecrement}>
         -1
       </button>
     </div>
