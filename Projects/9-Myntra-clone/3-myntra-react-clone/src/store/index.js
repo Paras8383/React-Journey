f07ead327a51d07b9bddc2f4cc235d5174c65cfe
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-11-28 21:00:35 +0530

    started working on redux

---
+++ Projects/9-Myntra-clone/3-myntra-react-clone/src/store/index.js
@@ -0,0 +1,9 @@
+import {createSlice, configureStore} from "@reduxjs/toolkit";
+import itemsSlice from "./itemsSlice";
+
+const myntraStore = configureStore({
+    reducer:{
+        items: itemsSlice.reducer
+    }
+});
+export default myntraStore;
